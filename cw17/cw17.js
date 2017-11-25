4f73ab95c2e594a063b65a546a2e73b380146f90
Pawe Szczeszek <pszczesz@gmail.com>
2017-11-25 10:49:48 +0100

    first 1

---
+++ cw17/cw17.js
@@ -0,0 +1,10 @@
+
+window.onload = function () {
+    var images = ["1.png", "2.png", "3.png"];
+    setInterval(function () {
+        document.getElementById("obrazek")
+                .setAttribute("src", "wylosowana z tab images");
+    }, 2000);
+
+};
+
